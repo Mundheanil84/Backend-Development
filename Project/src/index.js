838f3dc8cf2fcd4691d3d14e26174d6420ad9e1f
Mundhe Anil <mundheanil84@gmail.com>
2025-09-03 17:42:55 +0530

    all changes

---
+++ Project/src/index.js
@@ -0,0 +1,25 @@
+
+import mongoose from "mongoose";
+import {DB_NAME} from "./constants";
+
+import express from "express";
+const app = express()
+
+( async () => {
+    try {
+        mongoose.connect(`${process.env.MONGODB_URL}`)
+        app.on("error",() =>{
+            console.log("ERRR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT,() =>{
+            console.log(`App is listening on port $ {process.env.PORT}`);
+        })
+        
+    } catch (error) {
+        console.error("ERROR:", error)
+        throw err
+        
+    }
+}) ()
